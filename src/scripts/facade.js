e213458735b7ffb2160ab281e1390e7b006eb725
hubpork <michel@porkhead.org>
2025-05-23 11:47:28 +0200

    Try Errror :-)

--- src/scripts/facade.js
+++ src/scripts/facade.js
@@ -1,34 +1,24 @@
 export default function initLazyYoutube() {
-	const youtube = document.querySelectorAll(".youtube");
+    const youtube = document.querySelectorAll<HTMLElement>(".youtube");
 
-	youtube.forEach((el) => {
-		// Wenn schon ein img drin ist, nix tun
-		if (el.querySelector("img")) return;
+    for (let i = 0; i < youtube.length; i++) {
+        const source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed + "/sddefault.jpg";
+        const image = new Image();
+        image.src = source;
+        image.addEventListener("load", function () {
+            youtube[i].appendChild(image);
+        });
 
-		const videoId = el.dataset.embed;
-		const altText = el.dataset.alt || "YouTube Video";
-
-		if (!videoId) return;
-
-		const source = `https://img.youtube.com/vi/${videoId}/sddefault.jpg`;
-		const image = new Image();
-		image.src = source;
-		image.alt = altText;
-
-		image.addEventListener("load", () => {
-			el.appendChild(image);
-		});
-
-		el.addEventListener("click", function() {
-			const iframe = document.createElement("iframe");
-			iframe.setAttribute("frameborder", "0");
-			iframe.setAttribute("allowfullscreen", "");
-			iframe.setAttribute(
-				"src",
-				`https://www.youtube.com/embed/${videoId}?rel=0&showinfo=0&autoplay=1`
-			);
-			this.innerHTML = "";
-			this.appendChild(iframe);
-		});
-	});
+        youtube[i].addEventListener("click", function() {
+            const iframe = document.createElement("iframe");
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allowfullscreen", "");
+            iframe.setAttribute(
+                "src",
+                "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1"
+            );
+            this.innerHTML = "";
+            this.appendChild(iframe);
+        });
+    }
 }
